089b38080bcea175c1d7cc0a5dc8cf4039c6f303
lugirard <girardlulu@gmail.com>
2023-11-28 13:55:45 +0100

    First Commit Lateral Menu

---
+++ src/app/lateral-menu/lateral-menu.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-lateral-menu',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './lateral-menu.component.html',
+  styleUrl: './lateral-menu.component.css'
+})
+export class LateralMenuComponent {
+
+}
